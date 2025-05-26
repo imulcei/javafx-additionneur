f28b492770ea4f1bf2746e3f4fb4d21af0a2d405
imulcei <lucie.fiorrito@proton.me>
2025-05-26 15:30:20 +0200

    TODO fait

--- additionneur/src/main/java/fr/afpa/App.java
+++ additionneur/src/main/java/fr/afpa/App.java
@@ -20,9 +20,13 @@ import javafx.stage.Stage;
  * JavaFX App
  */
 public class App extends Application {
+    private int currentSum = 0;
+    private int currentInput = 0;
+    private boolean isOperator = false;
 
     @Override
     public void start(Stage stage) {
+
         stage.setTitle("Additionneur");
 
         TextArea textArea = new TextArea();
@@ -33,51 +37,30 @@ public class App extends Application {
         scrollPane.setPrefWidth(300);
         scrollPane.setPrefHeight(150);
 
-        // TODO serait-il possible d'imaginer une solution avec une boucle ?
-        GridPane gridNumbers = new GridPane();
-        Button button0 = new Button("0");
-        Button button1 = new Button("1");
-        Button button2 = new Button("2");
-        Button button3 = new Button("3");
-        Button button4 = new Button("4");
-        Button button5 = new Button("5");
-        Button button6 = new Button("6");
-        Button button7 = new Button("7");
-        Button button8 = new Button("8");
-        Button button9 = new Button("9");
+        EventHandler<ActionEvent> numberButtonHandler = event -> {
+            Button source = (Button) event.getSource();
+            String numberString = source.getText();
+            textArea.appendText(numberString);
+            if (isOperator) {
+                currentInput = 0;
+                isOperator = false;
+            }
+            currentInput = currentInput * 10 + Integer.parseInt(numberString);
+        };
 
-        // TODO idem remarque 36
-        gridNumbers.add(button0, 0, 0);
-        gridNumbers.add(button1, 1, 0);
-        gridNumbers.add(button2, 2, 0);
-        gridNumbers.add(button3, 3, 0);
-        gridNumbers.add(button4, 4, 0);
-        gridNumbers.add(button5, 0, 1);
-        gridNumbers.add(button6, 1, 1);
-        gridNumbers.add(button7, 2, 1);
-        gridNumbers.add(button8, 3, 1);
-        gridNumbers.add(button9, 4, 1);
+        GridPane gridNumbers = new GridPane();
+        Button[] buttons = new Button[10];
+        for (int i = 0; i <= 9; i++) {
+            buttons[i] = new Button(String.valueOf(i));
+            int row = i / 5;
+            int col = i % 5;
+            gridNumbers.add(buttons[i], col, row);
+            buttons[i].setOnAction(numberButtonHandler);
+        }
 
         gridNumbers.setHgap(20);
         gridNumbers.setVgap(10);
         gridNumbers.setAlignment(Pos.CENTER);
-
-        EventHandler<ActionEvent> numberButtonHandler = event -> {
-            Button source = (Button) event.getSource();
-            textArea.appendText(source.getText());
-        };
-
-        // TODO idem boucle
-        button0.setOnAction(numberButtonHandler);
-        button1.setOnAction(numberButtonHandler);
-        button2.setOnAction(numberButtonHandler);
-        button3.setOnAction(numberButtonHandler);
-        button4.setOnAction(numberButtonHandler);
-        button5.setOnAction(numberButtonHandler);
-        button6.setOnAction(numberButtonHandler);
-        button7.setOnAction(numberButtonHandler);
-        button8.setOnAction(numberButtonHandler);
-        button9.setOnAction(numberButtonHandler);
 
         Button clearButton = new Button("Vider");
         Button calculateButton = new Button("Calculer");
@@ -88,6 +71,9 @@ public class App extends Application {
             @Override
             public void handle(ActionEvent event) {
                 textArea.clear();
+                currentSum = 0;
+                currentInput = 0;
+                isOperator = false;
             }
         });
 
@@ -95,14 +81,11 @@ public class App extends Application {
             @Override
             public void handle(ActionEvent event) {
                 try {
-                    String calculString = textArea.getText();
-                    // TODO pour optimisation, serait-il possible d'imaginer une solution basée sur un attribut qui serait mis-à jour au fur et à mesure des saisies utilisateur plutôt qu'une analyse de la chaîne de caractère?
-                    String[] numberParts = calculString.split("\\+");
-                    int sum = 0;
-                    for (String numberPart : numberParts) {
-                        sum += Integer.parseInt(numberPart.trim());
-                    }
-                    textArea.appendText(" = " + sum);
+                    currentSum += currentInput;
+                    textArea.appendText(" = " + currentSum);
+                    currentInput = 0;
+                    currentSum = 0;
+                    isOperator = false;
                 } catch (NumberFormatException e) {
                     textArea.appendText("Erreur dans le calcul.");
                 }
@@ -123,6 +106,9 @@ public class App extends Application {
             @Override
             public void handle(KeyEvent event) {
                 if (event.getCode() == KeyCode.ADD || event.getCode() == KeyCode.PLUS) {
+                    currentSum += currentInput;
+                    currentInput = 0;
+                    isOperator = true;
                     textArea.appendText("+");
                 }
             }
